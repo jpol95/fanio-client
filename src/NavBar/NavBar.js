8a5a57b5bec70a4424f261d760cb81e713fd54ec
Jesse Pollack <jesse.lincolndouglas@gmail.com>
2020-12-20 01:40:01 -0500

    log out and log in works!

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./NavBar";
+import TokenService from "../Services/token-service";
+import { Link } from "react-router-dom";
 
 export default class NavBar extends React.Component {
   render() {
     return (
       <header>
-        <div className="navbar">Nav Bar</div>
+        <div className="navbar">
+          Nav Bar
+          {TokenService.hasAuthToken() ? (
+            <Link onClick={() => {TokenService.clearAuthToken(); window.open("/login", "_self")}}>Log Out</Link>
+          ) : (
+            <Link to="/login">Log In</Link>
+          )}
+        </div>
       </header>
     );
   }
