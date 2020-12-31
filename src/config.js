c2e77116f6a148e21bdd891f8b21e645aa211d95
Jesse Pollack <jesse.lincolndouglas@gmail.com>
2020-12-31 16:09:26 -0500

    trying to deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   API_KEY: process.env.REACT_APP_API_KEY,
-  API_ENDPOINT: "http://localhost:8000/api",
+  API_ENDPOINT: "https://murmuring-taiga-68517.herokuapp.com/api",
   TOKEN_KEY: "fanio-client-auth-token",
 };
